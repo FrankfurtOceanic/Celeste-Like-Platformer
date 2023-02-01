8dee6f9dcfe644b8b5b648d4d7332503cd08efd5
Joey Marten <joey.marten@mail.mcgill.ca>
2023-02-01 11:22:26 -0500

    Created Voronoi Shadergraph custom node

---
+++ Celeste-Like Platformer/Assets/VoronoiFunction.hlsl
@@ -0,0 +1,92 @@
+#ifndef MYHLSLINCLUDE_INCLUDE
+#define MYHLSLINCLUDE_INCLUDE
+
+inline float2 unity_voronoi_noise_randomVector (float2 UV, float offset)
+{
+    float2x2 m = float2x2(15.27, 47.63, 99.41, 89.98);
+    UV = frac(sin(mul(UV, m)) * 46839.32);
+    return float2(sin(UV.y*+offset)*0.5+0.5, cos(UV.x*offset)*0.5+0.5);
+}
+
+void UnityVoronoiF1_float(float2 UV, float AngleOffset, float CellDensity, out float Out, out float Cells)
+{
+    float2 g = floor(UV * CellDensity);
+    float2 f = frac(UV * CellDensity);
+    float t = 8.0;
+    float3 res = float3(8.0, 0.0, 0.0);
+
+    for(int y=-1; y<=1; y++)
+    {
+        for(int x=-1; x<=1; x++)
+        {
+            float2 lattice = float2(x,y);
+            float2 offset = unity_voronoi_noise_randomVector(lattice + g, AngleOffset);
+            float d = distance(lattice + offset, f);
+            if(d < res.x)
+            {
+                res = float3(d, offset.x, offset.y);
+                Out = res.x;
+                Cells = res.y;
+            }
+        }
+    }
+}
+
+void UnityVoronoiF2_float(float2 UV, float AngleOffset, float CellDensity, out float Out, out float Cells)
+{
+    float2 g = floor(UV * CellDensity);
+    float2 f = frac(UV * CellDensity);
+    float t = 8.0;
+    float4 res = float4(8.0, 8.0, 0.0, 0.0);
+
+    for(int y=-1; y<=1; y++)
+    {
+        for(int x=-1; x<=1; x++)
+        {
+            float2 lattice = float2(x,y);
+            float2 offset = unity_voronoi_noise_randomVector(lattice + g, AngleOffset);
+            float d = distance(lattice + offset, f);
+            if(d < res.x)
+            {
+                res = float4(d, res.x, offset.x, offset.y);
+                Out = res.y;
+                Cells = res.z;
+            }
+            else if(d < res.y)
+            {
+                res = float4(res.x, d, offset.x, offset.y);
+                Out = res.y;
+            }
+        }
+    }
+}
+
+void UnityVoronoiEdge_float(float2 UV, float AngleOffset, float CellDensity, out float Out, out float Cells)
+{
+    float2 g = floor(UV * CellDensity);
+    float2 f = frac(UV * CellDensity);
+    float t = 8.0;
+    float4 res = float4(8.0, 8.0, 0.0, 0.0);
+
+    for(int y=-1; y<=1; y++)
+    {
+        for(int x=-1; x<=1; x++)
+        {
+            float2 lattice = float2(x,y);
+            float2 offset = unity_voronoi_noise_randomVector(lattice + g, AngleOffset);
+            float d = distance(lattice + offset, f);
+            if(d < res.x)
+            {
+                res = float4(d, res.x, offset.x, offset.y);
+                Out = res.y-res.x;
+                Cells = res.z;
+            }
+            else if(d < res.y)
+            {
+                res = float4(res.x, d, offset.x, offset.y);
+                Out = res.y-res.x;
+            }
+        }
+    }
+}
+#endif
